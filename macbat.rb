ee064ac5d4872bb3337881c80b985f3f3387329c
zeleza <mail@zeleza.ru>
2025-07-06 15:57:50 +0300

    Документация и формула Homebrew: - README: уточнена инструкция установки через brew (ручной запуск после установки), добавлены команды для обновления - README: улучшена инструкция по сборке из исходников — добавлен шаг установки Xcode CLT, исправлены шаги запуска

--- macbat.rb
+++ macbat.rb
@@ -1,15 +1,15 @@
 class Macbat < Formula
   desc "Утилита мониторинга аккумулятора (binary)"
   homepage "https://github.com/qzeleza/macbat"
-  version "v2.1.3"
+  version "v2.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/qzeleza/macbat/releases/download/v2.1.3/macbat-darwin-arm64.tar.gz"
-      sha256 "af9597dc07d4f97c22499ca813965b08f7fbec2dc936d9de843619c120322ea1"
+      url "https://github.com/qzeleza/macbat/releases/download/v2.1.4/macbat-darwin-arm64.tar.gz"
+      sha256 "555574f326553864cca3640e623c06e7d82cdf86262fc21801bde9aa6dc20a28"
     else
-      url "https://github.com/qzeleza/macbat/releases/download/v2.1.3/macbat-darwin-amd64.tar.gz"
-      sha256 "1c054503dad649c2e0e8cbb240394ccdd4a4d958dd9b94a1fb118ee0e409d507"
+      url "https://github.com/qzeleza/macbat/releases/download/v2.1.4/macbat-darwin-amd64.tar.gz"
+      sha256 "f937ebfc23fa63257dd465401bcafac8e9491da6ae9444b2d66271f8dd8c0d38"
     end
   end
 
